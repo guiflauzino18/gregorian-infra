50013c752614a03cb3edeed86fd91ec27540c35b
guiflauzino18 <guilhermegf16@gmail.com>
2024-11-01 10:12:18 -0300

    Ajuste userData.sh passando -y no install e instalando mysql client

--- infra/env/prod/userData.sh
+++ infra/env/prod/userData.sh
@@ -3,7 +3,7 @@
 #Instala Docker
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -15,6 +15,9 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 #Instala GitHub Agent
+
+#Instala Mysql Client
+sudo apt install -y mysql-client-core-8.0
